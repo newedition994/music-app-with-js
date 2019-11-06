7adc49110cd9064fe5cacc39ed100339a36b9493
Breyanna Vass <breyannavass@gmail.com>
2019-11-06 15:10:59 -0500

    adding bubbles to go with taps

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ window.addEventListener("load", () => {
   const sounds = document.querySelectorAll(".sound");
   const pads = document.querySelectorAll(".pads div");
 
+  const visual = document.querySelector(".visual");
+  const colors = [
+    rgb(185, 66, 245),
+    rgb(66, 212, 245),
+    rgb(96, 139, 61),
+    rgb(245, 66, 108),
+    rgb(239, 245, 66),
+    rgb(81, 66, 245)
+  ];
+
   pads.forEach((pad, index) => {
     pad.addEventListener("click", function() {
       sounds[index].currentTime = 0;
